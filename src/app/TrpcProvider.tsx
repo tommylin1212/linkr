881001dfb6c9bea7a4ce8d62d8655af490e5a0f9
Caleb Tomlin <caleb.w.tomlin@gmail.com>
2023-04-17 14:30:35 -0500

    fix localhost issue hopefully

--- src/app/TrpcProvider.tsx
+++ src/app/TrpcProvider.tsx
@@ -7,13 +7,19 @@ import {useState} from "react"
 import superjson from "superjson";
 import {trpc} from "~/trpc"
 
+const getBaseUrl = () => {
+    if (typeof window !== "undefined") return ""; // browser should use relative url
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+    return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  };
+
 export const TrpcProvider: React.FC<{children: React.ReactNode}> = p => {
     const [queryClient] = useState(() => new QueryClient())
     const [trpcClient] = useState(() =>
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: "http://localhost:3000/api/trpc"
+                    url: `${getBaseUrl()}/api/trpc`
                 })
             ],
             transformer: superjson
